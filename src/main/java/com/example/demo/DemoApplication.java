fba049e11a265316094b731d0757fb0993bbce15
Aditya1028 <adityaa750@gmail.com>
2021-07-01 12:26:06 +0530

    commit

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -5,10 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-
-
 @SpringBootApplication
 public class DemoApplication extends SpringBootServletInitializer{	
+	
 	@Override
 	  protected SpringApplicationBuilder configure(SpringApplicationBuilder builder){
 	    return builder.sources(DemoApplication.class);
