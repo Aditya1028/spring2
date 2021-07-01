3a0fb0ab92353c85c89fb373939bafc77470c000
Aditya1028 <adityaa750@gmail.com>
2021-07-01 11:57:39 +0530

    adding

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -8,7 +8,7 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 
 
 @SpringBootApplication
-public class DemoApplication  extends SpringBootServletInitializer{	
+public class DemoApplication extends SpringBootServletInitializer{	
 	@Override
 	  protected SpringApplicationBuilder configure(SpringApplicationBuilder builder){
 	    return builder.sources(DemoApplication.class);
